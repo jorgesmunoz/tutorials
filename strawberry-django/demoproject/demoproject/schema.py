b14c7d6bced94d682be2cd53d88f9b5d911238ab
jorgesmunoz <jorgesmunoz@gmail.com>
2022-04-12 17:55:48 -0300

    demoproject

---
+++ strawberry-django/demoproject/demoproject/schema.py
@@ -0,0 +1,12 @@
+# schema.py
+import strawberry
+from typing import List
+from .types import Fruit
+
+
+@strawberry.type
+class Query:
+    fruits: List[Fruit] = strawberry.django.field()
+
+
+schema = strawberry.Schema(query=Query)
---
+++ strawberry-django/demoproject/demoproject/types.py
@@ -0,0 +1,19 @@
+# types.py
+import strawberry
+from strawberry.django import auto
+from typing import List
+from . import models
+
+
+@strawberry.django.type(models.Fruit)
+class Fruit:
+    id: auto
+    name: auto
+    color: 'Color'
+
+
+@strawberry.django.type(models.Color)
+class Color:
+    id: auto
+    name: auto
+    fruits: List[Fruit]
